31c222df991f9e9aa8055b86815b852974f323d3
Software Engineer <software.engineer.student.acc@gmail.com>
2026-02-05 19:50:26 +0300

    fix: fix flake8 error

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from django.core.management.utils import get_random_secret_key
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
